74887f5bb05fef9dbb99b51ad6bc17bf0b6451ee
cranberryandmixnuts <sujin83668252@gmail.com>
2026-04-22 09:23:51 +0900

    new Outline shader

---
+++ Assets/Core/Test/OutLine/OutlineMaskCameraSync.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public sealed class OutlineMaskCameraSync : MonoBehaviour
+{
+    [SerializeField] private Camera sourceCamera = default;
+    [SerializeField] private Camera maskCamera = default;
+    [SerializeField] private Material compositeMaterial = default;
+    [SerializeField] private int depthBufferBits = 24;
+
+    private RenderTexture maskTexture = default;
+    private int cachedWidth;
+    private int cachedHeight;
+
+    private void OnEnable()
+    {
+        SyncCamera();
+        RecreateMaskTextureIfNeeded();
+        ApplyMaterialTexture();
+    }
+
+    private void LateUpdate()
+    {
+        SyncCamera();
+        RecreateMaskTextureIfNeeded();
+        ApplyMaterialTexture();
+    }
+
+    private void OnDisable() => ReleaseMaskTexture();
+
+    private void SyncCamera()
+    {
+        Transform sourceTransform = sourceCamera.transform;
+        Transform maskTransform = maskCamera.transform;
+
+        maskTransform.SetPositionAndRotation(sourceTransform.position, sourceTransform.rotation);
+
+        maskCamera.orthographic = sourceCamera.orthographic;
+        maskCamera.fieldOfView = sourceCamera.fieldOfView;
+        maskCamera.orthographicSize = sourceCamera.orthographicSize;
+        maskCamera.nearClipPlane = sourceCamera.nearClipPlane;
+        maskCamera.farClipPlane = sourceCamera.farClipPlane;
+        maskCamera.aspect = sourceCamera.aspect;
+        maskCamera.allowHDR = false;
+        maskCamera.allowMSAA = false;
+        maskCamera.clearFlags = CameraClearFlags.SolidColor;
+        maskCamera.backgroundColor = Color.black;
+    }
+
+    private void RecreateMaskTextureIfNeeded()
+    {
+        int width = Mathf.Max(1, sourceCamera.pixelWidth);
+        int height = Mathf.Max(1, sourceCamera.pixelHeight);
+
+        if (maskTexture != null && width == cachedWidth && height == cachedHeight)
+            return;
+
+        ReleaseMaskTexture();
+
+        maskTexture = new RenderTexture(width, height, depthBufferBits, RenderTextureFormat.R8)
+        {
+            name = "_OutlineMaskRT",
+            filterMode = FilterMode.Point,
+            wrapMode = TextureWrapMode.Clamp,
+            useMipMap = false,
+            autoGenerateMips = false
+        };
+
+        maskTexture.Create();
+
+        cachedWidth = width;
+        cachedHeight = height;
+
+        maskCamera.targetTexture = maskTexture;
+    }
+
+    private void ApplyMaterialTexture() => compositeMaterial.SetTexture("_OutlineMaskTex", maskTexture);
+
+    private void ReleaseMaskTexture()
+    {
+        if (maskTexture == null)
+            return;
+
+        maskCamera.targetTexture = null;
+
+        if (Application.isPlaying)
+            Destroy(maskTexture);
+        else
+            DestroyImmediate(maskTexture);
+
+        maskTexture = null;
+        cachedWidth = 0;
+        cachedHeight = 0;
+    }
+}
---
+++ Assets/Core/Test/OutLine/ScreenSpaceOutlineComposite.shader
@@ -0,0 +1,86 @@
+Shader "Hidden/Custom/ScreenSpaceOutlineComposite"
+{
+    Properties
+    {
+        [NoScaleOffset]_OutlineMaskTex("Outline Mask", 2D) = "black" {}
+        _OutlineColor("Outline Color", Color) = (0, 0, 0, 1)
+        _OutlineWidthPixels("Outline Width Pixels", Range(0, 8)) = 8
+        _AlphaThreshold("Alpha Threshold", Range(0, 1)) = 0.5
+    }
+
+    SubShader
+    {
+        Tags
+        {
+            "RenderPipeline" = "UniversalPipeline"
+        }
+
+        Pass
+        {
+            Name "ScreenSpaceOutlineComposite"
+
+            ZWrite Off
+            ZTest Always
+            Cull Off
+            Blend Off
+
+            HLSLPROGRAM
+            #pragma target 4.5
+            #pragma vertex Vert
+            #pragma fragment Frag
+
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+
+            TEXTURE2D(_OutlineMaskTex);
+            SAMPLER(sampler_OutlineMaskTex);
+
+            float4 _OutlineColor;
+            float _OutlineWidthPixels;
+            float _AlphaThreshold;
+            float4 _OutlineMaskTex_TexelSize;
+
+            float SampleMask(float2 uv)
+            {
+                return SAMPLE_TEXTURE2D(_OutlineMaskTex, sampler_OutlineMaskTex, uv).r;
+            }
+
+            half4 Frag(Varyings input) : SV_Target
+            {
+                UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+
+                float2 uv = input.texcoord;
+                half4 sceneColor = SAMPLE_TEXTURE2D_X(_BlitTexture, sampler_LinearClamp, uv);
+
+                float center = step(_AlphaThreshold, SampleMask(uv));
+                float outer = 0.0;
+                float2 texel = _OutlineMaskTex_TexelSize.xy;
+                int radius = (int)clamp(round(_OutlineWidthPixels), 0.0, 8.0);
+
+                [unroll]
+                for (int y = -8; y <= 8; y++)
+                {
+                    [unroll]
+                    for (int x = -8; x <= 8; x++)
+                    {
+                        if (x == 0 && y == 0)
+                            continue;
+
+                        if (x * x + y * y > radius * radius)
+                            continue;
+
+                        float2 sampleUv = uv + float2((float)x, (float)y) * texel;
+                        float sampleValue = step(_AlphaThreshold, SampleMask(sampleUv));
+                        outer = max(outer, sampleValue);
+                    }
+                }
+
+                float outline = saturate(outer - center);
+                sceneColor.rgb = lerp(sceneColor.rgb, _OutlineColor.rgb, outline * _OutlineColor.a);
+
+                return sceneColor;
+            }
+            ENDHLSL
+        }
+    }
+}
